6102daf1ac8644c71c0611709928fcf87da5d0bd
Daniekeys-official <68125935+Daniekeys@users.noreply.github.com>
2021-05-01 19:40:14 +0100

    now

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,44 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  // we created a function so filter out the one with the corresponding id and leave the remaining
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours);
+  }
+  const fetchTours = async () => {
+    setLoading(true);
+   try {
+     
+     const response = await fetch(url);
+     const tours = await response.json();
+     setLoading(false);
+     setTours(tours);
+   } catch (error) {
+     setLoading(false);
+     console.log(error);
+   }
+    
+  }
+  useEffect(()=> {
+    fetchTours();
+  }, []);
+  if(loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  return (
+    <main>
+      {/* we just need to passs in the remove tour as an property to the other children */}
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+    
+  ) 
 }
 
 export default App
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>WELCOME TO DANIEKEYS TOURS</h2>
+        
+      </div>
+      <div>
+        {tours.map((tour)=> {
+          return (
+            <Tour key={tour.id} {...tour} removeTour={removeTour} >
+
+            </Tour>
+          )
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
